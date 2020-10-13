e75d28bb40efe7eb2906a6bc5fefe82a1a3fb7b1
ch010402 <christoph.latzer@gmail.com>
2020-10-13 12:14:17 +0200

    Update read-temp.cpp

--- read-temp.cpp
+++ read-temp.cpp
@@ -8,31 +8,41 @@ string device = "28-3c01a81688f4";
 string basedir = "/sys/bus/w1/devices/";
 string subdir = "/w1_slave";
 string text;
-double temp = 123.45;
+double temp = -123.45;
+char buffer_in[265];
+
+double get_temp (string dev_address) {
+  //open file and check if openend
+  FILE *devFile = fopen(dev_address, "r");
+  if (devFile == NULL) {
+    fclose(devFile);
+    cout << "Unable to open file" << dev_address << endl;
+    return 1;
+  }
+  //define variable
+  double temperatur = 987.65;
+  int tempInt;
+  char crcConf[5];
+  //read file
+  fscanf(devFile, "%*x %*x %*x %*x %*x %*x %*x %*x %*x : crc=%*x %s", crcConf);
+  if (strncmp(crcConf, "YES", 3) == 0) {
+    fscanf(devFile, "%*x %*x %*x %*x %*x %*x %*x %*x %*x t=%5d", tempInt);
+    temperatur = tempInt / 1000.0;
+  }
+  fclose(devFile);
+  
+  return temperatur;
+}
+
 
 int main (void) {
-  cout << "Starting..." << endl;
+  cout << "Start readning Temp-Sensor ..." << endl;
   string address = basedir + device + subdir;
+  
+  temp = get_temp(address);
+  
+  cout << "Temperatur: " << temp << endl;
 
-  ifstream file( address );
-  while (getline (file, text)) {
-    cout << text << endl;
-/*  // ACHTUNG bricht bei der 2. Linie ab, fixen
-    if ( text.find("YES") == string::npos) {
-      cout << "checksum error, temperatur not read" << endl;
-      break; 
-    }
-*/
-/*    if ( text.find("t=") != string::npos) {
-      int pos = text.find("t=");
-      cout << "pos: " << pos << endl;
-      // int temp_raw = 
-    } */
-    cout << text.find("t=") << endl;
-  }
-  file.close();
-
- // cout << text << endl;
   cout << "Done." << endl;
   return 0;
 }
