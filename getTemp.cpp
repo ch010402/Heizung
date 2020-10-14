2aa92cf1048c7e98b214d95aebc10f9d4b75310b
ch010402 <christoph.latzer@gmail.com>
2020-10-14 11:22:37 +0200

    Update getTemp.cpp

--- getTemp.cpp
+++ getTemp.cpp
@@ -63,7 +63,7 @@ double getTemp(string deviceAddress, bool debug = false) {
   return temp;
 }
 
-int main(bool debug = false){
+int main(int debug = 0){
 
   string testSensor = "28-3c01a81688f4";
   
