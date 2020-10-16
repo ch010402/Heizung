8a7aaff49c81e97fe05a1da7881e18d10830af22
ch010402 <christoph.latzer@gmail.com>
2020-10-16 18:05:56 +0200

    update

---
+++ sandbox/ofen-warmwasser.cpp
@@ -0,0 +1,12 @@
+/*
+MIT license 
+ch010402 16.10.2020
+*/
+
+#include <wiringPi.h> // library to access the GPIO Pins on a raspberryPI !!depriciated!!
+#include <iostream>   // cout
+#include <fstream>    // file stream access
+#include <string>     // string class
+#include <sstream>    // string stream needed for file access to put data into string
+
+using namespace std;
