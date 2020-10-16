8e5011da484c614926eaa0d12bf98631196f95c9
ch010402 <christoph.latzer@gmail.com>
2020-10-16 20:26:05 +0200

    test first class

--- sandbox/ofen-warmwasser.cpp
+++ sandbox/ofen-warmwasser.cpp
@@ -10,3 +10,50 @@ ch010402 16.10.2020
 #include <sstream>    // string stream needed for file access to put data into string
 
 using namespace std;
+
+//----- classes
+class pump {
+  public:
+    int pin;
+    // constructor
+    pump(int p) {
+      pin = p;
+    }
+    // methodes
+    void on() {
+      bool newStatus = true;
+      if (!initialized) initialize();
+      if (newStatus == oldStatus) return;
+      digitalWrite(pin, LOW);
+      cout << "Pumpe eingeschaltet" << endl;
+      oldStatus = newStatus;
+    }
+    void off() {
+      bool newStatus = false;
+      if (!initialized) initialize();
+      if (newStatus = oldStatus) return;
+      digitalWrite(pin, HIGH);
+      cout << "Pumpe ausgeschaltet" << endl;
+      oldStatus = newStatus;
+    }
+    private:
+      bool initialized = false; // default false 
+      bool oldStatus = false; // false = off true = on; default false as initialized
+      void initialize() {
+        wiringPiSetup();
+        pinMode(pin,OUTPUT);
+        initialized = true;
+      }
+};
+
+int main(void) {
+  // test setup
+  pump boilerpumpe(21);
+  while (true)
+  {
+    boilerpumpe.on();
+    delay(5*1000);
+    boilerpumpe.off();
+    delay(5*1000);
+  }
+}
