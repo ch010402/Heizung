1640a21582d3eaed796c15f73179e17a33ff4e69
ch010402 <christoph.latzer@gmail.com>
2020-11-30 08:02:43 +0100

    convert filename to string from argv

--- sandbox/logger.cpp
+++ sandbox/logger.cpp
@@ -20,26 +20,15 @@ int main(int argc, char** argv){
   string formated_now = date::format("%F %X %Z", now);
   cout << formated_now << endl;
   
-  site_t fileNamePos = argv[0].find("/");
+  string fullFileName = argv[0];
+  size_t fileNamePos = fullFileName.find("/");
   if (fileNamePos == string::npos) {
     cout << "file name not found, abort" << endl;
     return -100;
   }
-  string fileName = argv[0].substr(fileNamePos+1);
+  string fileName = fullFileName.substr(fileNamePos+1);
   
-  cout << filename << endl;
-
-  /*
-  size_t TempPos = data.find("t=");
-      if (TempPos == string::npos) {
-        cout << "failed to find value -> abort!" << endl;
-        return -102;
-      }
-      string strTemp = data.substr(TempPos+2);
-      double temp = stod(strTemp)/1000;
-      return temp;
-    }
-  */
+  cout << fileName << endl;
   
   return 0;
 }
