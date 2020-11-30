9c1e0766e106045e24d07489d4a5a1946efa76ff
ch010402 <christoph.latzer@gmail.com>
2020-11-30 07:56:28 +0100

    get file name without leading ./

--- sandbox/logger.cpp
+++ sandbox/logger.cpp
@@ -19,7 +19,27 @@ int main(int argc, char** argv){
   auto now = chrono::system_clock::now(); 
   string formated_now = date::format("%F %X %Z", now);
   cout << formated_now << endl;
-  cout << argv[0] << endl;
+  
+  site_t fileNamePos = argv[0].find("/");
+  if (fileNamePos == string::npos) {
+    cout << "file name not found, abort" << endl;
+    return -100;
+  }
+  string fileName = argv[0].substr(fileNamePos+1);
+  
+  cout << filename << endl;
 
+  /*
+  size_t TempPos = data.find("t=");
+      if (TempPos == string::npos) {
+        cout << "failed to find value -> abort!" << endl;
+        return -102;
+      }
+      string strTemp = data.substr(TempPos+2);
+      double temp = stod(strTemp)/1000;
+      return temp;
+    }
+  */
+  
   return 0;
 }
