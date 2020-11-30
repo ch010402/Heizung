1cdf9e143a80c972e4b86816d45e58f1901335fb
ch010402 <christoph.latzer@gmail.com>
2020-11-30 07:00:22 +0100

    first draft of logger

---
+++ sandbox/logger.cpp
@@ -0,0 +1,24 @@
+/*
+MIT license 
+created: ch010402 23.11.2020
+  learning:
+changed: 
+*/
+
+
+#include <iostream>   // cout
+#include <chrono>
+#include "include/date.h"
+#include <string>
+
+using namespace std;
+
+
+int main(void){
+
+  auto now = chrono::system_clock::now(); 
+  string formated_now = date::format("%F %X %Z", now);
+  cout << formated_now << endl;
+
+  return 0;
+}
