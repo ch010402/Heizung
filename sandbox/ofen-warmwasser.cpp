9f047ae7b8d525e09eed27aa113fe6dd46c6ecdc
ch010402 <christoph.latzer@gmail.com>
2021-01-04 20:33:29 +0100

    internal logger class

--- sandbox/ofen-warmwasser.cpp
+++ sandbox/ofen-warmwasser.cpp
@@ -10,10 +10,87 @@ changed: ch010402 23.11.2020 mixup pump and valve IO corrected
 #include <fstream>    // file stream access
 #include <string>     // string class
 #include <sstream>    // string stream needed for file access to put data into string
+#include <chrono>         // get the syste
+#include "include/date.h" // format the time https://howardhinnant.github.io/date/date.html#to_stream_formatting
+#include <unistd.h>       // used for usleep
 
 using namespace std;
 
 //----- classes
+class Logger{
+  public:
+  string argv_0;
+  //constructor
+  Logger(string filename){
+    argv_0 = filename;
+  }
+  // methodes
+  void log(string msg) {
+    // check if inizilized, if not do
+    if (!initialized) initialize();
+    // open or create file 
+    ofstream logfile;
+    logfile.open(file, ios::app);
+    // write message with timestapm to file
+    if (logfile.is_open()){
+      logfile << timestamp() << " : " << msg << "\n";
+    }
+    else cout << "Unable to open file";
+    int size = logfile.tellp();
+    // close file
+    logfile.close();
+    cout << "log entry written, file size "<< size <<" Byte \n";
+    // check if filesitze is bigger than 1M bytes
+    if (size > 1000000) {
+      // exit(EXIT_FAILURE);
+      initialized = false;
+    }
+  }
+  private:
+  string timestamp(){
+    // get the current timestamp with timezone
+    auto now = chrono::system_clock::now();
+    string ts = date::format("%F %X %Z", now);
+    // the timezone is UTC TODO: change to locale tz
+    return ts;
+  }
+  string today(){
+    // get the date of today
+    auto now = chrono::system_clock::now();
+    string td = date::format("%F", now);
+    return td;
+  }
+  string filename(string argv){
+    // compose the filename from the executable file the current date and .log 
+    string fullFileName = argv;
+    size_t fileNamePos = fullFileName.find_last_of("/");
+    if (fileNamePos == string::npos) {
+      cout << "file name not found, abort" << endl;
+      return "nofilename";
+    }
+    string file = fullFileName.substr(fileNamePos+1);
+    string fn = file + "_" + today() + ".log";
+    return fn;
+  }
+  bool initialized = false;
+  string file;
+  void initialize(){
+    // inzilialize = create the filename 
+    file = filename(argv_0);
+    int i = 1;
+    // if the filename already exists create a new one with -itterator 
+    while (filesystem::exists(file)){ 
+      string s = to_string(i);
+      int minus = file.length() - file.find_last_of("-");
+      if (i > 1) file = file.erase(file.size()-minus);
+      else file = file.erase(file.size()-4);
+      file = file + "-" + s + ".log";
+      i++;
+    }
+    initialized = true;
+  }
+};
+
 class pump {
   public:
     int pin;
@@ -146,8 +223,9 @@ temperaturSensor::temperaturSensor(string str) {
   path = baseDir + device + tempFile;
 }
 
-int main(void) {
+int main(int argc, char** argv) {
   // test setup
+  Logger log(argv[0]);
   pump boilerpumpe(28);
   valve boilervalve(21);
   temperaturSensor testSensor1("28-3c01a81688f4");
